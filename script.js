91ed332a8ce734a5cd2a23025485a9ab21560da7
Aidan Stritch <aidan.stritch@gmail.com>
2019-05-18 09:38:54 +0000

    added another example of sideways traversing

--- script.js
+++ script.js
@@ -20,13 +20,23 @@ $(document).ready(function() {
 	  	$(".stream3").addClass('highlight_stream');
 	});
 
+
+	/*example of traversing up/down the DOM*/
 	$("p").click(function(){
 		$(this).children("a").css("background-color", "yellow"); 
 		/* returns all the <a> child elements that are within this paragraph*/
 	});
+	
+	/*example of traversing sideways in the DOM*/
 	$(".bottom_button").click(function(){
 		$(this).prev().slideToggle('slow');
 		/*Toggle the visibility of the paragraph when a button is clicked*/
+	});
+	
+	/*example of traversing sideways in the DOM (uses sidewaysExample.html)*/
+	$("img").click(function() {
+		$(this).next().children("p").slideDown();
+		/*Open the paragraph once the image is clicked*/
 	});
 	
 }); 
